a32517c462d0975c8e2ab0d7520045af10102694
Caesar <caesar.hinlo.tech@gmail.com>
2021-06-21 10:17:28 +0800

    [ER]Imported icons from material ui for header

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+  return (
+    //change app to
+    <div className="app">
+      <h1>Caesar Hinlo Slack Clone</h1>
+
+      <Header />
+      {/* SideBar*/}
+      {/* React Router ->Chat screen*/}
+    </div>
+  );
+}
+
+export default App;
