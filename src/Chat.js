7eef713b5c87d0911aac1fa3f004ef62d524c1d0
Caesar <caesar.hinlo.tech@gmail.com>
2021-06-23 12:03:55 +0800

    [ER]Modification for Chat reflects room name

--- src/Chat.js
+++ src/Chat.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Chat.css";
 import { useParams } from "react-router-dom";
 import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
 import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import db from "./firebase";
 
 export default function Chat() {
   const { roomId } = useParams();
+  const [roomDetails, setRoomDetails] = useState(null);
+
+  useEffect(() => {
+    if (roomId) {
+      db.collection("rooms")
+        .doc(roomId)
+        .onSnapshot((snapshot) => {
+          setRoomDetails(snapshot.data());
+        });
+    }
+  }, [roomId]);
+
   return (
     <div className="chat">
       <div className="chat__header">
         <div className="chat__headerLeft">
           <h4 className="chat__channelName">
-            <strong>#{roomId}</strong>
+            <strong>#{roomDetails?.name}</strong>
           </h4>
           <StarBorderOutlinedIcon />
         </div>
