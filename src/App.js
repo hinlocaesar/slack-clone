ca8bd0e925abde63a8b1eb5364b579bdc4a6bc24
Caesar <caesar.hinlo.tech@gmail.com>
2021-06-25 11:43:36 +0800

    [Er]Save design for Chat.js

--- src/App.js
+++ src/App.js
@@ -4,24 +4,34 @@ import Sidebar from "./Sidebar";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Chat from "./Chat";
 import Message from "./Message";
+import React, { useState } from "react";
+
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
     //change app to
     <div className="app">
       <Router>
-        <Header />
-        <div className="app__body">
-          <Sidebar />
+        {!user ? (
+          <h1> Login page</h1>
+        ) : (
+          <>
+            <Header />
+            <div className="app__body">
+              <Sidebar />
 
-          <Switch>
-            <Route path="/room/:roomId">
-              <Chat />
-            </Route>
-            <Route path="/">
-              <h1>Welcome</h1>
-            </Route>
-          </Switch>
-        </div>
+              <Switch>
+                <Route path="/room/:roomId">
+                  <Chat />
+                </Route>
+                <Route path="/">
+                  <h1>Welcome</h1>
+                </Route>
+              </Switch>
+            </div>
+          </>
+        )}
       </Router>
     </div>
   );
